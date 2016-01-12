72759b6dc5a995ca9470f979cadda224bae8d2ec
victor29 <victor29@github.com>
2016-01-11 21:31:15 -0500

    Primera prueba

---
+++ muy_basico.py
@@ -0,0 +1,26 @@
+# -*- coding: UTF-8 -*-
+
+#Cosas simples en python - 1
+
+#Operaciones basicas
+
+print (0+1)
+print (1 + 1)
+print (3 * 1)
+print (8 / 2)
+print (((2 * 5) + 40 / 4) / 4)
+print (13 / 2)
+
+#Division entera 
+
+print (13 // 2)
+
+#modulo
+
+print ("modulo  " + str(21 % 5))
+
+#potencia
+
+print ("potencia  " + str(5 ** 2))
+
+
---
+++ primero.py
@@ -0,0 +1,10 @@
+# -*- coding: UTF-8 -*-
+
+#Prueba en python numero 1
+
+mundo = True
+if mundo:
+	print ("Tené cuidado de no caerte")
+
+
+
